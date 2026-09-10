f232ce3abad6f9899d31d9397b60bfb09d24998e
BananaJoe1984 <akirk12@lsu.edu>
2026-09-10 16:04:25 -0500

    font change

--- lib/main.dart
+++ lib/main.dart
@@ -104,7 +104,10 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: .center,
           children: [
-            const Text('Aiden has pushed the button this many times:'),
+            const Text(
+              'Aiden pushed the button this many times:',
+              style: TextStyle(fontSize: 24),
+            ),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
